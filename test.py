9e7e6e555a0b2284e5eec960ed14dfcfbf0e0917
Chris Smith <chris87@gmail.com>
2016-07-10 07:05:01 +0100

    Add support for --user, improve tests.
    Make the tests use nose instead of unittest, and use properly
    parameterized arguments. This makes it a lot easier to see
    what fails.

--- test.py
+++ test.py
@@ -1,37 +1,32 @@
 #!/usr/bin/python3
 
 import subprocess
-import unittest
+from nose_parameterized import parameterized
 
 
-class RerunTest(unittest.TestCase):
+def _run(cmd):
+    subprocess.call(cmd,
+                    stdout=subprocess.DEVNULL,
+                    stderr=subprocess.DEVNULL)
 
 
-    def _run(self, cmd):
-        subprocess.call(cmd,
-                        stdout=subprocess.DEVNULL,
-                        stderr=subprocess.DEVNULL)
-
-
-    def test_command_matches(self):
-        commands = [
-            'docker run --name=test123 -d hello-world',
-            'docker run --name=test123 -d hello-world:latest',
-            'docker run --name=test123 -d hello-world /hello world...',
-            'docker run --name=test123 --restart=always -d hello-world',
-            'docker run --name=test123 --restart=on-failure:10 -d hello-world',
-            'docker run --name=test123 --net=host -d hello-world',
-            'docker run --name=test123 -d -p=127.0.0.1:443:443/tcp -p=127.0.0.1::1336/tcp hello-world',
-            'docker run --name=test123 -d -p=443/tcp hello-world',
-            'docker run --name=test123 --volume=/dev/null:/null --volume=/dev/urandom:/mnt/random -d hello-world',
-        ]
-
-        for command in commands:
-            with self.subTest(cmd=command):
-                self._run(['docker', 'rm', '-f', 'test123'])
-                self._run(command.split(' '))
-                output = subprocess.check_output(['./docker-rerun', '--dry-run', 'test123'])
-                output = output.decode('utf-8').strip().splitlines()
-                self.assertEqual(output[3], command)
-                self._run(['docker', 'rm', '-f', 'test123'])
+@parameterized([
+    'docker run --name=test123 -d hello-world',
+    'docker run --name=test123 -d hello-world:latest',
+    'docker run --name=test123 -d hello-world /hello world...',
+    'docker run --name=test123 --restart=always -d hello-world',
+    'docker run --name=test123 --restart=on-failure:10 -d hello-world',
+    'docker run --name=test123 --net=host -d hello-world',
+    'docker run --name=test123 -d -p=127.0.0.1:443:443/tcp -p=127.0.0.1::1336/tcp hello-world',
+    'docker run --name=test123 -d -p=443/tcp hello-world',
+    'docker run --name=test123 --user=root -d hello-world /hello foobar',
+    'docker run --name=test123 --volume=/dev/null:/null --volume=/dev/urandom:/mnt/random -d hello-world',
+])
+def test_command_matches(command):
+    _run(['docker', 'rm', '-f', 'test123'])
+    _run(command.split(' '))
+    output = subprocess.check_output(['./docker-rerun', '--dry-run', 'test123'])
+    output = output.decode('utf-8').strip().splitlines()
+    assert output[3] == command
+    _run(['docker', 'rm', '-f', 'test123'])
 
